d7af8d1a66ae15411fd9e4977e1ae5514486b72d
Carl Kashnier <Tetum007@gmail.com>
2016-02-21 10:40:05 -0500

    fix errors in the unix return// & a large rewrite of the code

--- index.js
+++ index.js
@@ -9,32 +9,35 @@ var urlObj = url.parse(req.url, true);
 var dateString = urlObj.href
 var dateString = dateString.replace(/%20/g,' ');
 var dateStringSan = sanitize(dateString.substring(1))
-var parsedDate = new Date(Date.parse(dateStringSan));
-var unixOut = Date.parse(dateStringSan)
-
-
-if (isNaN(dateStringSan)) {
-if (parsedDate == "Invalid Date" || unixOut === "NaN"){
-  parsedDate = null
-  unixOut = null
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify({"unix": unixOut, "natural": parsedDate}));
-  res.end();
-  return
-}else{
-  var natDate = dateformat(unixOut, "mmmm d, yyyy")
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify({"unix": unixOut, "natural": natDate}));
-  res.end();
-  return
+var parsedDate = new Date(Date.parse(dateStringSan))
+//date strhing should be MMMM DD, YYYY or #
+if (isNaN(dateStringSan)){
+  if (parsedDate == "Invalid Date"){
+    parsedDate = null
+    unixOut = null
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({"unix": unixOut, "natural": parsedDate}));
+    res.end();
+    return
+  } else {
+//Is a valid Date
+var natDate = dateformat(parsedDate, "mmmm d, yyyy")
+var unixOut = Date.parse(dateStringSan)/1000;
+res.writeHead(200, { 'Content-Type': 'application/json' });
+res.write(JSON.stringify({"unix": unixOut, "natural": natDate}));
+res.end();
+return
 }
-} else {
-  var natDate = dateformat(unixOut, "mmmm d, yyyy")
-unixOut = dateStringSan
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify({"unix": unixOut, "natural": natDate}));
-  res.end();
-  return
+}  else {
+//Is a unix date code
+var t = new Date(dateStringSan * 1000);
+var natDate = dateformat(t, "mmmm d, yyyy")
+//var natDate = dateformat(parsedDate, "mmmm d, yyyy")
+res.writeHead(200, { 'Content-Type': 'application/json' });
+res.write(JSON.stringify({"unix": dateStringSan, "natural": natDate}));
+res.end();
+return
 }
+
 });
 server.listen(process.env.PORT || 8888);
